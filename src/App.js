b96f0c0b3f66e5277fc5b96a556daa92540d4edc
Rdmi-503KtibJ <123767853+Rdmi-503KtibJ@users.noreply.github.com>
2023-11-22 20:51:58 -0600

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,61 @@
 import logo from './ss.svg';
 import './App.css';
-
-import React , {useState } from 'react';
+import TaskList from './TaskList';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
 
-  const handleAddTask = () =>{
-    setTasks([...tasks, {id: tasks.length + 1, text: newTask}]);
+  const handleAddTask = () => {
+    setTasks([...tasks, { id: tasks.length + 1, text: newTask }]);
     setNewTask('');
   };
 
   return (
-    <div>
-          <img src = {logo} alt = "Logo" width ="450" height = "450" />{/*Use imported logo*/}
+    <Router>
+      <div>
+        <img src={logo} alt="Logo" width="450" height="450" />
 
-    <h1>Task Manager</h1>
-    <ul>
-      {tasks.map((task) => (
-        <li key={task.id}>{task.text}</li>
-      ))}
-    </ul>
-    <input
-      type="text"
-      placeholder="New task"
-      value={newTask}
-      onChange={(e) => setNewTask(e.target.value)}
-    />
-    <button onClick={handleAddTask}>Add Task</button>
-  </div>
+        <h1>Task Manager</h1>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/tasks">View Tasks</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/tasks" component={TaskList} />
+          <Route
+            path="/"
+            render={() => (
+              <div>
+                <ul>
+                  {tasks.map((task) => (
+                    <li key={task.id}>{task.text}</li>
+                  ))}
+                </ul>
+                <input
+                  type="text"
+                  placeholder="New task"
+                  value={newTask}
+                  onChange={(e) => setNewTask(e.target.value)}
+                />
+                <button onClick={handleAddTask}>Add Task</button>
+              </div>
+            )}
+            exact
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/TaskList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TaskList =() =>{
+
+    return (
+        <div>
+            <h2>Task List </h2>
+            {/*Add youre htask listing here */}
+        </div>
+    );
+};
+
+
+export default TaskList;
